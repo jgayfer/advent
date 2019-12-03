8469341365dfc02b4f356f29173ef9ab20f6383e
James Gayfer <gayfer.james@gmail.com>
2019-12-02 19:44:04 -0800

    2019 day 1 part 2

--- 2019/01/solution.rb
+++ 2019/01/solution.rb
@@ -17,3 +17,17 @@ class PartOne
     @input_file ||= File.read('input.txt')
   end
 end
+
+class PartTwo < PartOne
+  def solve
+    module_masses.sum do |mass|
+      adjusted_fuel(mass)
+    end
+  end
+
+  def adjusted_fuel(mass)
+    fuel = fuel(mass)
+    return 0 if fuel < 0
+    fuel + adjusted_fuel(fuel)
+  end
+end
