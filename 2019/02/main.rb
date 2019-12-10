4b9a060248fe59d4572e2605b22f989521df4fa4
James Gayfer <gayfer.james@gmail.com>
2019-12-09 23:05:05 -0800

    Use new computer to find solution

--- 2019/02/main.rb
+++ 2019/02/main.rb
@@ -1,4 +1,30 @@
-require_relative 'solution'
+require_relative 'computer'
 
-puts "Part 1: #{PartOne.new.solve}"
-puts "Part 2: #{PartTwo.new.solve}"
+og_input = File.read('input.txt').split(',').map(&:to_i)
+
+puts "--- Part 1 ---"
+
+input = og_input.dup
+input[1] = 12
+input[2] = 2
+
+computer = Computer.new(input)
+computer.compute
+puts computer.memory[0]
+
+puts "--- Part 2 ---"
+
+0.upto(99) do |noun|
+  0.upto(99) do |verb|
+    input = og_input.dup
+    input[1] = noun
+    input[2] = verb
+
+    computer = Computer.new(input)
+    computer.compute
+
+    if computer.memory[0] == 19690720
+      break puts 100 * noun + verb
+    end
+  end
+end
